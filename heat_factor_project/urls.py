e6bf11a61a5f85b829b1c6cc9a149241bcd41c88
rickey <splitnines@gmail.com>
2020-02-21 07:18:25 -0800

    added heat_factor/urls.py and changed the url routing

--- heat_factor_project/urls.py
+++ heat_factor_project/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from heat_factor import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('heat_factor/', views.heat_factor),
+    path('', include('heat_factor.urls')),
 ]
