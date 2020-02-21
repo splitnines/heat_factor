014dcd2013752f4c99640c645ac4c5b0d47f5e40
rickey <splitnines@gmail.com>
2020-02-21 08:31:15 -0800

    changed ALLOWED_HOSTS in settings.py to * for heroku deployment

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'veto58a+*z$x-!wc)(sp%n=rt3am-^y=9^v3ubn4orpc@&pr_7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
