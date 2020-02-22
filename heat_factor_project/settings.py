09e56d8bcfc05120094df5a7a8e18f1c9e66ca1b
rickey <splitnines@gmail.com>
2020-02-22 14:32:10 -0800

    more settings.py changes troubleshooting heroku static files issue

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'heat_factor_project.urls'
