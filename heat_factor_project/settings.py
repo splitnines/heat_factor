f5a61504be398c6057d0c6718047b20bec5b9b25
rickey <splitnines@gmail.com>
2020-03-19 13:55:00 -0700

    updated url regex in views.py to accept www.

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'veto58a+*z$x-!wc)(sp%n=rt3am-^y=9^v3ubn4orpc@&pr_7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
