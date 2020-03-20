c2a5f8367ce426b21b8509dee462749275a4438a
rickey <splitnines@gmail.com>
2020-03-19 18:06:48 -0700

    added django-heroku package in an attampt to fix the static file issue

--- heat_factor/apps.py
+++ heat_factor/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class HeatFactorConfig(AppConfig):
     name = 'heat_factor'
+
+#class GetUppedConfig(AppConfig):
+#    name = 'get_upped'
--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'heat_factor.apps.HeatFactorConfig'
+    'heat_factor.apps.HeatFactorConfig',
+    #'get_upped.apps.GetUppedConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+import django_heroku
+django_heroku.settings(locals())
