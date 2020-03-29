d757dc856a06cdbb34e659a19fcb815b1cab379a
Rickey Ingrassia <splitnines@gmail.com>
2020-03-29 06:21:52 -0700

    added html code to points.html

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
-#DEBUG = True
-#DEBUG = False
+
 
 #ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = [str(os.environ.get('ALLOWED_HOSTS'))]
