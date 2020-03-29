4a64b0848a710c7ff96feefa48205a3ad737e166
Rickey Ingrassia <splitnines@gmail.com>
2020-03-28 22:06:47 -0700

    last settings.py clean-up...i hope

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'veto58a+*z$x-!wc)(sp%n=rt3am-^y=9^v3ubn4orpc@&pr_7'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
