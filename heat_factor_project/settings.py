917b53b7bea1951623ae43cac79797bd43dc8b5b
Rickey Ingrassia <splitnines@gmail.com>
2020-04-02 07:49:54 -0700

    hard coded DEBUG and ALLOWED_HOSTS in setting.py

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE', False)
+#DEBUG = os.environ.get('DEBUG_VALUE', False)
 #DEBUG = True
-#DEBUG = False
+DEBUG = False
 
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = [str(os.environ.get('ALLOWED_HOSTS'))]
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = [str(os.environ.get('ALLOWED_HOSTS'))]
 
 
 # Application definition
