a880b6c197eb46a27df886ca80f10cb8f50aafe2
Rickey Ingrassia <splitnines@gmail.com>
2020-04-03 11:00:55 -0700

    fuck

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DEBUG_VALUE')
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
