69836aa7b0a8b405ee459545bc515b592ed5602c
Rickey Ingrassia <splitnines@gmail.com>
2020-05-26 19:27:32 -0700

    tweaking js code, nbd

--- static/js/highlight_btn.js
+++ static/js/highlight_btn.js
@@ -1,18 +1,16 @@
 window.onload = function () {
-    var elms = document.getElementsByTagName('input');
+    var elms = document.getElementsByTagName("input");
 
     // search for element with class "btn"
     for (var i = 0; i < elms.length; i++) {
-
-        if (elms[i].getAttribute('class') === 'btn') {
-
+        if (elms[i].getAttribute("class") === "btn") {
             elms[i].onmouseover = function () {
-                this.style.backgroundColor = '#ededed';
-            }
+                this.style.backgroundColor = "#ededed";
+            };
 
             elms[i].onmouseout = function () {
-                this.style.backgroundColor = '#cfcfcf';
-            }
+                this.style.backgroundColor = "#cfcfcf";
+            };
         }
     }
-}
+};
