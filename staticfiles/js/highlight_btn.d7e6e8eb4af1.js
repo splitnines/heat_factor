209a6431e9bc6f7ab93ad0ea5039731170b85fbf
Rickey Ingrassia <splitnines@gmail.com>
2020-05-28 07:21:19 -0700

    chnages how mouseover/mouseout behaves

--- staticfiles/js/highlight_btn.d7e6e8eb4af1.js
+++ staticfiles/js/highlight_btn.d7e6e8eb4af1.js
@@ -6,11 +6,11 @@ window.onload = function () {
     for (var i = 0; i < elms.length; i++) {
         if (elms[i].getAttribute("class") === "btn") {
             elms[i].onmouseover = function () {
-                this.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+                this.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
             };
 
             elms[i].onmouseout = function () {
-                this.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+                this.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
             };
         }
     }
