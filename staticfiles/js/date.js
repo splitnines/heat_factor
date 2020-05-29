9ab1f5c410917a50e89bf8ae82b25c95c2f6a067
Rickey Ingrassia <splitnines@gmail.com>
2020-05-29 10:33:10 -0700

    changed timestamp format

--- staticfiles/js/date.js
+++ staticfiles/js/date.js
@@ -5,5 +5,15 @@ function retDate() {
     return myDate;
 };
 
+function timeStamp() {
+    const today = new Date();
+
+    let ts = today.toDateString() + ' ';
+    ts += today.getHours() + ':' + today.getMinutes() + ':';
+    ts += today.getSeconds();
+
+    return ts;
+};
+
 var el = document.getElementById('date');
-el.textContent = retDate();
+el.textContent = timeStamp();
