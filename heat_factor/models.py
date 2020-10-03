330104cdffc0e24fbabc42bed289fc5eadc7d5f6
Rickey Ingrassia <splitnines@gmail.com>
2020-10-02 21:13:53 -0700

    found and fixed a bug in the models config

--- heat_factor/models.py
+++ heat_factor/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Uspsa(models.Model):
-    uspsa_num = models.CharField(max_length=10, unique=True)
+    uspsa_num = models.CharField(max_length=10)
     division = models.CharField(max_length=20)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True, editable=True)
