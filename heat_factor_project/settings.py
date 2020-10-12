9f545ecadf11050aaf34d4f9a02bd621715ecafc
Rickey Ingrassia <splitnines@gmail.com>
2020-10-12 07:09:43 -0700

    removed pagination setting from settings.py

--- heat_factor_project/settings.py
+++ heat_factor_project/settings.py
@@ -148,7 +148,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    # 'PAGE_SIZE': 10
 }
 
 django_heroku.settings(locals())
