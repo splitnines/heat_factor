d24cfffb33988f7f80a36d9d7ec3ac769ed0419f
Rickey Ingrassia <splitnines@gmail.com>
2021-06-04 08:42:54 -0700

    changed gridiron model to editable fields

--- grid_iron/models.py
+++ grid_iron/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Gridiron(models.Model):
-    team_name = models.CharField(max_length=100, unique=True)
-    team_mem1 = models.CharField(max_length=20, unique=True)
-    team_mem2 = models.CharField(max_length=20, unique=True)
-    team_mem3 = models.CharField(max_length=20, unique=True)
+    team_name = models.CharField(max_length=100, unique=True, editable=True)
+    team_mem1 = models.CharField(max_length=20, unique=True, editable=True)
+    team_mem2 = models.CharField(max_length=20, unique=True, editable=True)
+    team_mem3 = models.CharField(max_length=20, unique=True, editable=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True, editable=True)
 
