19fedae0c8c5dd9bce90aeee5378584b1269b95d
Rickey Ingrassia <splitnines@gmail.com>
2022-03-31 12:13:57 -0700

    fixed not

--- heat_factor/pointspersecond.py
+++ heat_factor/pointspersecond.py
@@ -172,7 +172,7 @@ def get_match_links(form_dict):
             # added 09/30/2020 because steel challenge matches broke shit
             'Steel Challenge' not in match_link_info['name'] and
             # added for FullMetalJess because it broke shit 03/31/2022
-            'Monster Match League' in match_link_info['name']
+            'Monster Match League' not in match_link_info['name']
         ):
             match_links_json.append(match_link_info)
 
