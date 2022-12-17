a3ce82e86468270a3bcaecd25ba487dce8484bcc
Rickey Ingrassia <splitnines@gmail.com>
2022-12-16 18:56:11 -0800

    commented out admin honeypot and removed package

--- heat_factor_project/urls.py
+++ heat_factor_project/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', include('admin_honeypot.urls',
-         namespace='admin_honeypot')),
+    # path('admin/', include('admin_honeypot.urls',
+    #      namespace='admin_honeypot')),
     path('mgnt', admin.site.urls),
     path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
     path('', include('heat_factor.urls')),
-    path('', include('admin_honeypot_fix.urls')),
+    # path('', include('admin_honeypot_fix.urls')),
     path('', include('grid_iron.urls')),
     path(
         'robots.txt',
